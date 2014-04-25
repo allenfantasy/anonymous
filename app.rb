847efc9e48151f3812b625edae9c2545e9dee926
Allen Wu <allenwu1018@gmail.com>
2014-04-26 00:32:49 +0800

    initial

---
+++ app.rb
@@ -0,0 +1,43 @@
+$:.unshift File.expand_path("../lib", __FILE__)
+
+require 'sinatra'
+require 'sinatra/devise'
+require 'sinatra/reloader' if development?
+require 'active_support/all'
+require 'multi_xml'
+require 'builder'
+
+configure do
+  enable :logging
+end
+
+get '/' do
+  logger.info "------GET HOME------"
+  logger.info params
+  "Hello Sinatra!"
+end
+
+get '/wechat' do
+  logger.info "------VERIFY------"
+  logger.info params
+  params["echostr"]
+end
+
+post '/wechat' do
+  logger.info "------POST------"
+  logger.info params
+  logger.info "------BODY------"
+  #logger.info "REQUEST CLASS: #{request.class}" # Sinatra::Request < Rack::Request
+  #logger.info "BODY: #{request.body}" # StringIO < Data < Object
+  #logger.info "BODY.READ: #{request.body.read}" # String
+  req_message = MultiXml.parse(request.body.read)['xml']
+  logger.info "REQ: #{req_message}"
+  authorize!
+
+  logger.info "PASSED"
+
+  # RESPONSE
+
+  query = req_message["Content"]
+
+end
