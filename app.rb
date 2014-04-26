3345813289fb667a9d967a00cb4ed7112d412601
Allen Wu <allenwu1018@gmail.com>
2014-04-26 14:27:39 +0800

    try

--- app.rb
+++ app.rb
@@ -11,8 +11,12 @@ require 'whenever'
 
 configure do
   enable :logging
+
+  set :appid, "wxcabc0cd7000f0d70"
+  set :appsecret, "757b330edc8ead6e7f6d552c32a9cd1a"
+
   # update the latest one
-  token_res = HTTParty.post("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=#{appid}&secret=#{secret}")
+  token_res = HTTParty.post("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=#{settings.appid}&secret=#{settings.appsecret}")
 
   logger.info token_res
 
