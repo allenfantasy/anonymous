2eab19771daee6f7398e92572c2789b96a64ff30
Allen Wu <allenwu1018@gmail.com>
2014-11-29 08:24:59 +0800

    refactor

--- app.rb
+++ app.rb
@@ -2,10 +2,9 @@
 $:.unshift File.expand_path("../lib", __FILE__)
 
 require 'sinatra'
-require 'sinatra/devise'
+require 'sinatra/wechat'
 require 'sinatra/reloader' if development?
 require 'active_support/all'
-require 'multi_xml'
 require 'builder'
 require 'pry'
 require './settings'
@@ -17,7 +16,6 @@ configure do
   Sinatra::Application.class_eval do
     env = self.settings.environment
     Settings[env].keys.each do |key|
-      puts key
       define_singleton key.to_sym do
         Settings[env][key]
       end
@@ -33,123 +31,132 @@ configure do
   set :token_timestamp, Time.now
 end
 
-queue = []
-sessions = []
+base_url '/wechat'
 
 get '/' do
-  logger.info "------GET HOME------"
-  logger.info params
   "Hello Sinatra!"
 end
 
-get '/wechat' do
-  logger.info "------VERIFY------"
-  logger.info params
-  authorize!
-  params["echostr"]
+queue = [] # 等待聊天的队列
+sessions = [] # 正在聊天的session
+
+def sessions.include_user?(name)
+  self.map{|s| s.values }.flatten.uniq.include? name
 end
 
-post '/wechat' do
-  req_msg = MultiXml.parse(request.body.read)['xml']
-  authorize!
+on :text do |params|
+  username = params["FromUserName"]
+  msg = params["Content"]
+  
+  in_session = sessions.include_user?(username)
+  in_queue = queue.include?(username)
 
-  # processing messages
-
-  uid = req_msg["FromUserName"]
-  req_origin_msg = req_msg["Content"]
-  logger.info "------INIT------"
-  logger.info "Queue: #{queue}"
-  logger.info "Sessions: #{sessions}"
-  #logger.info "Access token: #{access_token}"
-  logger.info "Token Response: #{settings.access_token}"
-
-  idx = sessions.map{|s| s[0]}.index(uid) || sessions.map{|s| s[1]}.index(uid)
-
-  if !idx.nil? # in sessions
-    if req_origin_msg == "bye"
-      # kill session
-      m = "[SYS]对话已结束"
-      session = sessions.delete_at(idx)
-      uid1 = session[0] == uid ? session[1] : session[0]
-
-      # push ending message to uid1
-      # update expired token if needed
-      if Time.now - settings.token_timestamp > 7200 # expired
-        token_res = HTTParty.post("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=#{settings.appid}&secret=#{settings.appsecret}")
-
-        set :access_token, token_res.parsed_response["access_token"]
-        set :token_timestamp, Time.now
-      end
-
-      res = HTTParty.post("https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=#{settings.access_token}", :body => {
-        :touser => uid1,
-        :msgtype => "text",
-        :text => {
-          :content => m
-        }
-      }.to_json)
-
-      logger.info "pushing msg..."
-      logger.info res
+  if msg == "START"
+    if in_session
+      # 正常发一个start
+    elsif in_queue 
+      # 提示用户耐心等待
+    else 
+      # 判断queue里是否有人，有的话建立session
+      # 没有的话将用户放到等待队列里
+    end
+  elsif msg == "BYE"
+    if in_session
+      # 结束当前session
+    elsif in_queue
+      # 将用户从queue里去掉
     else
-      # repost
-      session = sessions[idx]
-      uid1 = session[0] == uid ? session[1] : session[0]
-      puts settings.access_token
-      res = HTTParty.post("https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=#{settings.access_token}", :body => {
-        :touser => uid1,
-        :msgtype => "text",
-        :text => {
-          :content => req_origin_msg
-        }
-      }.to_json)
+      # 提示用户应该用start进入聊天
+    end
+  else
+    if in_session
+      # 正常聊天，将信息转发给对方
+    elsif in_queue
+      # 提示用户耐心等待
+    else
+      # 提示用户应该用start进入聊天
+    end
+  end
+  "LOOOOL"
+  #if sessions.include_user?(username)
+    #"Congrats! You are in!"
+    #if msg == "bye"
+      ## kill session
+      #m = "[SYS]对话已结束"
+      #session = sessions.delete_at(idx)
+      #uid1 = session[0] == uid ? session[1] : session[0]
 
-      logger.info "------REPOST------"
-      logger.info "Queue: #{queue}"
-      logger.info "Sessions: #{sessions}"
+      ## push ending message to uid1
+      ## update expired token if needed
+      #if Time.now - settings.token_timestamp > 7200 # expired
+        #token_res = HTTParty.post("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=#{settings.appid}&secret=#{settings.appsecret}")
+
+        #set :access_token, token_res.parsed_response["access_token"]
+        #set :token_timestamp, Time.now
+      #end
+
+      #res = HTTParty.post("https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=#{settings.access_token}", :body => {
+        #:touser => uid1,
+        #:msgtype => "text",
+        #:text => {
+          #:content => m
+        #}
+      #}.to_json)
 
       #logger.info "pushing msg..."
       #logger.info res
-    end
-  else
-    if req_origin_msg == "start"
-      if queue.include?(uid)
-        # duplicate request
-        m = "[SYS]你已在等待队列中啦，请耐心等待另一位陌生人"
-      elsif queue.empty?
-        # no available chatter
-        m = "[SYS]你已进入等待队列！请耐心等待~"
-        queue.push(uid)
-      else
-        m = "[SYS]已建立对话"
-        uid1 = queue.shift
+    #else
+      ## repost
+      #session = sessions[idx]
+      #uid1 = session[0] == uid ? session[1] : session[0]
+      #puts settings.access_token
+      #res = HTTParty.post("https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=#{settings.access_token}", :body => {
+        #:touser => uid1,
+        #:msgtype => "text",
+        #:text => {
+          #:content => req_origin_msg
+        #}
+      #}.to_json)
 
-        # push msg to uid1
-        res = HTTParty.post("https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=#{settings.access_token}", :body => {
-          :touser => uid1,
-          :msgtype => "text",
-          :text => {
-            :content => m
-          }
-        }.to_json)
+      #logger.info "------REPOST------"
+      #logger.info "Queue: #{queue}"
+      #logger.info "Sessions: #{sessions}"
 
-        logger.info "pushing msg..."
-        logger.info res
+      ##logger.info "pushing msg..."
+      ##logger.info res
+    #end
+  #else
+    #"You are not in the session!"
+    #if req_origin_msg == "start"
+      #if queue.include?(uid)
+        ## duplicate request
+        #m = "[SYS]你已在等待队列中啦，请耐心等待另一位陌生人"
+      #elsif queue.empty?
+        ## no available chatter
+        #m = "[SYS]你已进入等待队列！请耐心等待~"
+        #queue.push(uid)
+      #else
+        #m = "[SYS]已建立对话"
+        #uid1 = queue.shift
 
-        # setup a session
-        sessions.push([uid, uid1])
-      end
-    else
-      m = "[SYS]请输入start开始聊天"
-    end
-  end
+        ## push msg to uid1
+        #res = HTTParty.post("https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token=#{settings.access_token}", :body => {
+          #:touser => uid1,
+          #:msgtype => "text",
+          #:text => {
+            #:content => m
+          #}
+        #}.to_json)
 
-  # setup return msg to request client
-  res_msg = {
-    "ToUserName" => req_msg["FromUserName"], "FromUserName" => req_msg["ToUserName"],
-    "CreateTime" => Time.now.to_i, # unix timestamp
-    "MsgType" => 'text',
-    "Content" => m
-  }.to_xml(:root => "xml")
+        #logger.info "pushing msg..."
+        #logger.info res
 
+        ## setup a session
+        #sessions.push([uid, uid1])
+      #end
+    #else
+      #m = "[SYS]请输入start开始聊天"
+    #end
+  #end
+  #"Hello back"
 end
--- lib/sinatra/wechat.rb
+++ lib/sinatra/wechat.rb
@@ -1,17 +1,100 @@
+require 'httparty'
+require 'multi_xml'
+require 'active_support/all'
+
 module Sinatra
   module Wechat
-    def on
-      # TODO
-      # on :text do
-      #   ...
-      # end
+    TOKEN = 'dxhackers'
+
+    class Message
+      attr_accessor :type, :my_account, :user_account, :content
+
+      def initialize(content, received_msg)
+        # String: text
+        # TODO: Hash: non-text, check the type
+        @type = 'text'
+        puts received_msg
+        @my_account = received_msg["ToUserName"]
+        @user_account = received_msg["FromUserName"]
+        @content = content
+      end
+
+      def to_xml
+        {
+          ToUserName: @user_account,
+          FromUserName: @my_account,
+          CreateTime: Time.now.to_i,
+          MsgType: @type,
+          Content: @content
+        }.to_xml(:root => "xml", :skip_types => true)
+      end
     end
 
-    def push
+    # use in main context
+    module RegisterMethods
+      def base_url(url=nil)
+        return Wechat.base_url if url.nil?
+        Wechat.base_url = url
+
+        # set basic route
+        
+        get url do
+          authorize!
+          params["echostr"]
+        end
+
+        post url do
+          authorize!
+          wechat_params = parse(request)
+          result = send(:"#{wechat_params['MsgType'].downcase}_handler", wechat_params)
+          puts result
+          return_msg = Wechat::Message.new(result, wechat_params)
+          return_msg.to_xml
+        end
+      end
+
+      def on(type)
+        return unless block_given?
+        type = type.to_s.downcase
+
+        HelperMethods.send :define_method, :"#{type}_handler" do |params|
+          yield params
+        end
+      end
+
+      def push
+      end
+
+      class << Wechat
+        attr_accessor :base_url
+      end
+    end
+    
+    # use in get/post/.. blocks
+    module HelperMethods
+      def authorize!
+        nonce, timestamp, signature = params[:nonce], params[:timestamp], params[:signature]
+        if [nonce, timestamp, signature].compact.size < 3 or signature != genarate_signature(TOKEN, nonce, timestamp)
+          logger.info "FAILED"
+          halt 401, "Forbidden! You Bastards!"
+        end
+      end
+      def parse(request)
+        MultiXml.parse(request.body.read)["xml"]
+      end
+
+      def method_missing(method, *args)
+        "Hello, this is the default message"
+      end
+
+      private
+      def genarate_signature(token, nonce, timestamp)
+        Digest::SHA1.hexdigest([token, nonce, timestamp].sort.join)
+      end
     end
 
-    def method_missing
-      # WAT
-    end
   end
+
+  register Wechat::RegisterMethods
+  helpers Wechat::HelperMethods
 end
